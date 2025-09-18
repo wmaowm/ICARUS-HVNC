62d5bd15531b46f05dcab573596700a3c1388366
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:49:12 +0400

    Class1.cs

---
+++ ICARUS HVNC/Hidden Application/Class1.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Collections;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+    internal class AeroListView : ListView
+    {
+        private const uint WM_CHANGEUISTATE = 0x127;
+
+        private const short UIS_SET = 1;
+        private const short UISF_HIDEFOCUS = 0x1;
+        private readonly IntPtr _removeDots = new IntPtr(NativeMethodsHelper.MakeWin32Long(UIS_SET, UISF_HIDEFOCUS));
+
+        private ListViewColumnSorter LvwColumnSorter { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AeroListView"/> class.
+        /// </summary>
+        public AeroListView()
+        {
+            SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint, true);
+            this.LvwColumnSorter = new ListViewColumnSorter();
+            this.ListViewItemSorter = LvwColumnSorter;
+            this.View = View.Details;
+            this.FullRowSelect = true;
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:HandleCreated" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+
+            if (PlatformHelper.RunningOnMono) return;
+
+            if (PlatformHelper.VistaOrHigher)
+            {
+                // set window theme to explorer
+                NativeMethods.SetWindowTheme(this.Handle, "explorer", null);
+            }
+
+            if (PlatformHelper.XpOrHigher)
+            {
+                // removes the ugly dotted line around focused item
+                NativeMethods.SendMessage(this.Handle, WM_CHANGEUISTATE, _removeDots, IntPtr.Zero);
+            }
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:ColumnClick" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="ColumnClickEventArgs"/> instance containing the event data.</param>
+        protected override void OnColumnClick(ColumnClickEventArgs e)
+        {
+            base.OnColumnClick(e);
+
+            // Determine if clicked column is already the column that is being sorted.
+            if (e.Column == this.LvwColumnSorter.SortColumn)
+            {
+                // Reverse the current sort direction for this column.
+                this.LvwColumnSorter.Order = (this.LvwColumnSorter.Order == SortOrder.Ascending)
+                    ? SortOrder.Descending
+                    : SortOrder.Ascending;
+            }
+            else
+            {
+                // Set the column number that is to be sorted; default to ascending.
+                this.LvwColumnSorter.SortColumn = e.Column;
+                this.LvwColumnSorter.Order = SortOrder.Ascending;
+            }
+
+            // Perform the sort with these new sort options.
+            if (!this.VirtualMode)
+                this.Sort();
+        }
+    }
+
+    public class ListViewColumnSorter : IComparer
+    {
+        /// <summary>
+        /// Specifies the column to be sorted
+        /// </summary>
+        private int _columnToSort;
+
+        /// <summary>
+        /// Specifies the order in which to sort (i.e. 'Ascending').
+        /// </summary>
+        private SortOrder _orderOfSort;
+
+        /// <summary>
+        /// Case insensitive comparer object
+        /// </summary>
+        private readonly CaseInsensitiveComparer _objectCompare;
+
+        /// <summary>
+        /// Class constructor.  Initializes various elements
+        /// </summary>
+        public ListViewColumnSorter()
+        {
+            // Initialize the column to '0'
+            _columnToSort = 0;
+
+            // Initialize the sort order to 'none'
+            _orderOfSort = SortOrder.None;
+
+            // Initialize the CaseInsensitiveComparer object
+            _objectCompare = new CaseInsensitiveComparer();
+        }
+
+        /// <summary>
+        /// This method is inherited from the IComparer interface.  It compares the two objects passed using a case insensitive comparison.
+        /// </summary>
+        /// <param name="x">First object to be compared</param>
+        /// <param name="y">Second object to be compared</param>
+        /// <returns>The result of the comparison. "0" if equal, negative if 'x' is less than 'y' and positive if 'x' is greater than 'y'</returns>
+        public int Compare(object x, object y)
+        {
+            // Cast the objects to be compared to ListViewItem objects
+            var listviewX = (ListViewItem)x;
+            var listviewY = (ListViewItem)y;
+
+            if (listviewX.SubItems[0].Text == ".." || listviewY.SubItems[0].Text == "..")
+                return 0;
+
+            // Compare the two items
+            var compareResult = _objectCompare.Compare(listviewX.SubItems[_columnToSort].Text,
+                listviewY.SubItems[_columnToSort].Text);
+
+            // Calculate correct return value based on object comparison
+            if (_orderOfSort == SortOrder.Ascending)
+            {
+                // Ascending sort is selected, return normal result of compare operation
+                return compareResult;
+            }
+            else if (_orderOfSort == SortOrder.Descending)
+            {
+                // Descending sort is selected, return negative result of compare operation
+                return (-compareResult);
+            }
+            else
+            {
+                // Return '0' to indicate they are equal
+                return 0;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the number of the column to which to apply the sorting operation (Defaults to '0').
+        /// </summary>
+        public int SortColumn
+        {
+            set { _columnToSort = value; }
+            get { return _columnToSort; }
+        }
+
+        /// <summary>
+        /// Gets or sets the order of sorting to apply (for example, 'Ascending' or 'Descending').
+        /// </summary>
+        public SortOrder Order
+        {
+            set { _orderOfSort = value; }
+            get { return _orderOfSort; }
+        }
+    }
+
+    public static class NativeMethodsHelper
+    {
+        private const int LVM_FIRST = 0x1000;
+        private const int LVM_SETITEMSTATE = LVM_FIRST + 43;
+
+        private const int WM_VSCROLL = 277;
+        private static readonly IntPtr SB_PAGEBOTTOM = new IntPtr(7);
+
+        public static int MakeWin32Long(short wLow, short wHigh)
+        {
+            return (int)wLow << 16 | (int)(short)wHigh;
+        }
+
+        public static void SetItemState(IntPtr handle, int itemIndex, int mask, int value)
+        {
+            NativeMethods.LVITEM lvItem = new NativeMethods.LVITEM
+            {
+                stateMask = mask,
+                state = value
+            };
+
+            NativeMethods.SendMessageListViewItem(handle, LVM_SETITEMSTATE, new IntPtr(itemIndex), ref lvItem);
+        }
+
+        public static void ScrollToBottom(IntPtr handle)
+        {
+            NativeMethods.SendMessage(handle, WM_VSCROLL, SB_PAGEBOTTOM, IntPtr.Zero);
+        }
+    }
+
+    /// <summary>
+    /// Provides access to Win32 API and Microsoft C Runtime Library (msvcrt.dll).
+    /// </summary>
+    public static class NativeMethods
+    {
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        internal struct LVITEM
+        {
+            public uint mask;
+            public int iItem;
+            public int iSubItem;
+            public int state;
+            public int stateMask;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string pszText;
+            public int cchTextMax;
+            public int iImage;
+            public IntPtr lParam;
+            public int iIndent;
+            public int iGroupId;
+            public uint cColumns;
+            public IntPtr puColumns;
+            public IntPtr piColFmt;
+            public int iGroup;
+        };
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        internal static extern IntPtr SendMessage(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, EntryPoint = "SendMessage")]
+        internal static extern IntPtr SendMessageListViewItem(IntPtr hWnd, uint msg, IntPtr wParam, ref LVITEM lParam);
+
+        [DllImport("user32.dll")]
+        internal static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, int vk);
+
+        [DllImport("user32.dll")]
+        internal static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        [DllImport("uxtheme.dll", CharSet = CharSet.Unicode)]
+        internal static extern int SetWindowTheme(IntPtr hWnd, string pszSubAppName, string pszSubIdList);
+    }
+
+    public static class PlatformHelper
+    {
+        /// <summary>
+        /// Initializes the <see cref="PlatformHelper"/> class.
+        /// </summary>
+        static PlatformHelper()
+        {
+            Win32NT = Environment.OSVersion.Platform == PlatformID.Win32NT;
+            XpOrHigher = Win32NT && Environment.OSVersion.Version.Major >= 5;
+            VistaOrHigher = Win32NT && Environment.OSVersion.Version.Major >= 6;
+            SevenOrHigher = Win32NT && (Environment.OSVersion.Version >= new Version(6, 1));
+            EightOrHigher = Win32NT && (Environment.OSVersion.Version >= new Version(6, 2, 9200));
+            EightPointOneOrHigher = Win32NT && (Environment.OSVersion.Version >= new Version(6, 3));
+            TenOrHigher = Win32NT && (Environment.OSVersion.Version >= new Version(10, 0));
+            RunningOnMono = Type.GetType("Mono.Runtime") != null;
+
+            Name = "Unknown OS";
+            using (var searcher = new ManagementObjectSearcher("SELECT Caption FROM Win32_OperatingSystem"))
+            {
+                foreach (ManagementObject os in searcher.Get())
+                {
+                    Name = os["Caption"].ToString();
+                    break;
+                }
+            }
+
+            Name = Regex.Replace(Name, "^.*(?=Windows)", "").TrimEnd().TrimStart(); // Remove everything before first match "Windows" and trim end & start
+            Is64Bit = Environment.Is64BitOperatingSystem;
+            FullName = $"{Name} {(Is64Bit ? 64 : 32)} Bit";
+        }
+
+        /// <summary>
+        /// Gets the full name of the operating system running on this computer (including the edition and architecture).
+        /// </summary>
+        public static string FullName { get; }
+
+        /// <summary>
+        /// Gets the name of the operating system running on this computer (including the edition).
+        /// </summary>
+        public static string Name { get; }
+
+        /// <summary>
+        /// Determines whether the Operating System is 32 or 64-bit.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if the Operating System is 64-bit, otherwise <c>false</c> for 32-bit.
+        /// </value>
+        public static bool Is64Bit { get; }
+
+        /// <summary>
+        /// Returns a indicating whether the application is running in Mono runtime.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if the application is running in Mono runtime; otherwise, <c>false</c>.
+        /// </value>
+        public static bool RunningOnMono { get; }
+
+        /// <summary>
+        /// Returns a indicating whether the Operating System is Windows 32 NT based.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if the Operating System is Windows 32 NT based; otherwise, <c>false</c>.
+        /// </value>
+        public static bool Win32NT { get; }
+
+        /// <summary>
+        /// Returns a value indicating whether the Operating System is Windows XP or higher.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if the Operating System is Windows XP or higher; otherwise, <c>false</c>.
+        /// </value>
+        public static bool XpOrHigher { get; }
+
+        /// <summary>
+        /// Returns a value indicating whether the Operating System is Windows Vista or higher.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if the Operating System is Windows Vista or higher; otherwise, <c>false</c>.
+        /// </value>
+        public static bool VistaOrHigher { get; }
+
+        /// <summary>
+        /// Returns a value indicating whether the Operating System is Windows 7 or higher.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if the Operating System is Windows 7 or higher; otherwise, <c>false</c>.
+        /// </value>
+        public static bool SevenOrHigher { get; }
+
+        /// <summary>
+        /// Returns a value indicating whether the Operating System is Windows 8 or higher.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if the Operating System is Windows 8 or higher; otherwise, <c>false</c>.
+        /// </value>
+        public static bool EightOrHigher { get; }
+
+        /// <summary>
+        /// Returns a value indicating whether the Operating System is Windows 8.1 or higher.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if the Operating System is Windows 8.1 or higher; otherwise, <c>false</c>.
+        /// </value>
+        public static bool EightPointOneOrHigher { get; }
+
+        /// <summary>
+        /// Returns a value indicating whether the Operating System is Windows 10 or higher.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if the Operating System is Windows 10 or higher; otherwise, <c>false</c>.
+        /// </value>
+        public static bool TenOrHigher { get; }
+    }
